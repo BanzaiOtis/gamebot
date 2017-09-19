327802e9eebb41d59e1975095a34d2c684871e19
BanzaiOtis <banzaiotis@gmail.com>
2017-09-19 16:21:32 -0700

    fixed bug with not defining last_call_time before 1st call

--- gamebot.py
+++ gamebot.py
@@ -61,6 +61,7 @@ def parse_slack_output(slack_rtm_output):
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
     max_inactive_time = 29 * 60 # 29 minutes in seconds
+    last_call_time = time.time()
     if slack_client.rtm_connect():
         print("StarterBot connected and running!")
         while True:
